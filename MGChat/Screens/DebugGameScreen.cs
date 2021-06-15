0dba26dbdc3db70b35883cef5552991062654812
Exxarah <sarahbeavan@gmail.com>
2021-06-15 18:02:41 +1200

    Setup DebugGameScreen

---
+++ MGChat/Screens/DebugGameScreen.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using MGChat.UI;
+using Microsoft.Xna.Framework;
+
+namespace MGChat.Screens
+{
+    public class DebugGameScreen : GameScreen
+    {
+        private UiManager _uiManager;
+        private int _updateFPSIndex, _drawFPSIndex;
+        public override void Initialize()
+        {
+            Name = "Debug";
+            IsActive = false;
+            IsPopup = true;
+
+            _uiManager = new UiManager(this);
+
+            _updateFPSIndex = _uiManager.Add(new ValueLabel(
+                "Fonts/Arcade_In_12", "Average UpdateFPS: ", "", Vector2.One, Util.UI.ObjAlign.Right, Util.UI.ObjAlign.Below
+            ));
+        }
+
+        public override void LoadAssets()
+        {
+            _uiManager.LoadContent(ScreenManager.ContentMgr);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            ((ValueLabel) _uiManager.Get(_updateFPSIndex)).ValueText = ScreenManager.UpdateFPS.ToString(CultureInfo.CurrentCulture);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            _uiManager.Draw(ScreenManager.Sprites);
+        }
+
+        public override void UnloadAssets() { }
+    }
+}
---
+++ MGChat/UI/ValueLabel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MGChat.UI
+{
+    public class ValueLabel : Label
+    {
+        private string _labelText;
+        public string ValueText;
+        public new string Text => _labelText + ValueText;
+        
+        public ValueLabel(string fontPath, string labelText, string valueText, Vector2 position, Util.UI.ObjAlign xAlign, Util.UI.ObjAlign yAlign) : base(fontPath, labelText, position, xAlign, yAlign)
+        {
+            _labelText = labelText;
+            ValueText = valueText;
+        }
+    }
+}
